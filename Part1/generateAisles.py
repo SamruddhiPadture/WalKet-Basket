a7587e45ecfe1c1d330007c50dc63f7b6f790551
Tejashree Date <46928494+TejashreeDate@users.noreply.github.com>
2020-05-17 01:10:27 +0530

    Update generateAisles.py

--- Part1/generateAisles.py
+++ Part1/generateAisles.py
@@ -2,24 +2,25 @@ import json
 import csv
 
 orderList = [] #list of all order numbers due to multiple files
-aisleList=[] #list of all aisle strings of multiple user--to be used for levenstein
+aisleList=[]   #list of all aisle strings of multiple user--to be used for levenstein
+data ={}       #mapping of orderNo and its ProductIds
+
 #apply looping for multiple inputs after this only otherwise orderList and aisleList will get reset everytime.
 
 fileList=['Desktop/ord1.json','Desktop/ord2.json']
 
 #with open("Desktop/ord1.json", "r") as read_file:
 for j in range(0,len(fileList)):
-    with open(fileList[i],"r") as read_file:
+    with open(fileList[j],"r") as read_file:
         jsonInput = json.load(read_file)                    #read json input file
         #print(jsonInput)
 
         orderList.append(jsonInput['orderNo'])
-        print("List of all ordernumbers from multiple json inputs till now:") #sam this is your orderNo list :P
+        print("List of all ordernumbers from multiple json inputs till now:")
         print(orderList)
 
-        productList=[]                                      #list of productIds for one user 
-        #data ={}                                           #mapping of orderNo and its ProductIds
-
+        productList=[]                                      #list of productIds for one user
+        
         for i in jsonInput['products']:
             productList.append(i['productId'])
 
@@ -36,7 +37,7 @@ for j in range(0,len(fileList)):
                         aisle.append(row[2])
                         break
             aisleList.append(''.join(aisle))
-            print(aisleList)  #sam this is your aisle strings chi list :P
+            print(aisleList)
 
 
 
